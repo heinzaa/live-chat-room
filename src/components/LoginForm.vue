b35d6e072241234c6360bc7877214243e11e940b
Marc Heintz <heintz.marc@web.de>
2022-03-14 23:31:00 +0100

    create Login and SignUp Form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      
+      <input type="email" required placeholder="email" v-model="email" />
+      <input type="password" required placeholder="password" v-model="password" />
+      <button>Log in </button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+
+   setup(){
+       const displyName = ref('');
+       const email = ref('');
+       const password = ref('');
+
+      const handleSubmit = () => {
+          console.log( email.value, password.value, handleSubmit)
+      }
+
+       return { email, password}
+   }
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <input type="text" required placeholder="display name" v-model="displyName" />
+      <input type="email" required placeholder="email" v-model="email" />
+      <input type="password" required placeholder="password" v-model="password" />
+      <button>Sign up </button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import useSignup from '../composables/useSignup'
+export default {
+
+   setup(){ 
+
+       const { error, signup } = useSignup()
+
+
+       // refs
+       const displyName = ref('');
+       const email = ref('');
+       const password = ref('');
+
+      const handleSubmit = async() => {
+        await signup(email.value, password.value, displyName.value);
+      }
+
+       return {displyName, email, password, handleSubmit}
+   }
+
+
+}
+</script>
+
+<style>
+
+</style>
